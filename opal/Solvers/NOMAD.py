1fb6c680a1feb93660674cce6bfde2ace62fcf16
Dominique Orban <dominique.orban@gmail.com>
2010-07-20 23:09:52 +0200

    Cleanups in Solvers/NOMAD.py.

--- opal/Solvers/NOMAD.py
+++ opal/Solvers/NOMAD.py
@@ -20,87 +20,118 @@ class Parameter:
 
     
 class NOMADBlackbox(BlackBox):
-    '''
+    """ 
     NOMADBlackbox is an implementation of BlackBox, it contains
     the description and the method of communication between 
     NOMAD solver and an executable blackbox.
     The descriptions include the executable file name
     The communicating methods are read_input and write_output
     Those are specialized to NOMAD solver
-    '''
-    def __init__(self,solver=None,model=None,fileName='blackbox.py',**kwargs):
-        BlackBox.__init__(self,solver=solver,model=model,**kwargs)
+    """
+
+    def __init__(self, solver=None, model=None, fileName='blackbox.py',
+                 **kwargs):
+
+        BlackBox.__init__(self, solver=solver, model=model, **kwargs)
         self.file_name = fileName
         self.surrogate = None
         pass
      
-    def read_input(self,*args,**kwargs):
+    def read_input(self, *args, **kwargs):
+        """
+        .. warning::
+
+            Document this method!!!
+        """
         inputValues = []
         paramValues = []
         #print args
         #print ""
         if len(args) < 1:
-            return (inputValues,paramValues)
+            return (inputValues, paramValues)
+
+        # Extract every words from the file and save to a list
         f = open(args[1])
-        map(lambda l: inputValues.extend(l.strip('\n').strip(' ').split(' ')), f.readlines()) # Extract every words from the file and save to a list
+        map(lambda l: inputValues.extend(l.strip('\n').strip(' ').split(' ')),
+                                         f.readlines())
         f.close()
-        return (inputValues,paramValues)
+        return (inputValues, paramValues)
     
-    def write_output(self,objectiveValue,constraintValues):
+    def write_output(self, objectiveValue, constraintValues):
+        """
+        .. warning::
+
+            Document this method!!!
+        """
         print >> sys.stdout, objectiveValue,
         if len(constraintValues) > 0:
             for i in range(len(constraintValues)):
-                print >> sys.stdout,constraintValues[i],
+                print >> sys.stdout, constraintValues[i],
             print ""
         return
    
     def generate_surrogate(self):
+        """
+        .. warning::
+
+            Document this method!!!
+        """
         return
 
     def generate_executable_file(self):
+        """
+        Generate Python code to play the role of black box executable.
+        """
         
         tab = ' '*4
-        blackboxFile = open(self.file_name,'w')
-        # To avoid the error compability of python version (local version intalled by user) and
-        # global version (system), we don't turn black box as a executable but call it by
-        # python blackbox.py
+        bb = open(self.file_name, 'w')
+        # To avoid the error compability of python version (local version
+        # intalled by user) and global version (system), we don't make black
+        # box an executable but call it via `python blackbox.py`
         # --------
         # or predifine config.python to the used python
         # rootPackage = config.__name__.replace('.config','')
-        #blackboxFile.write(config.python + '\n')
-        #blackboxFile.write('#!/usr/bin/env python\n')
-        blackboxFile.write('import os\n')
-        blackboxFile.write('import sys\n')
-        blackboxFile.write('import string\n')
-        blackboxFile.write('import shutil\n')
-        blackboxFile.write('import pickle\n')
-        blackboxFile.write('from opal.Solvers.NOMAD import NOMADBlackbox\n')
-        #blackboxFile.write('from ' + rootPackage + '.core import modeldata\n')
-        #blackboxFile.write('from ' + rootPackage + '.core import blackbox\n')
+        #bb.write(config.python + '\n')
+        bb.write('#!/usr/bin/env python\n')
+        bb.write('import os\n')
+        bb.write('import sys\n')
+        bb.write('import string\n')
+        bb.write('import shutil\n')
+        bb.write('import pickle\n')
+        bb.write('from opal.Solvers.NOMAD import NOMADBlackbox\n')
+
+        #bb.write('from ' + rootPackage + '.core import modeldata\n')
+        #bb.write('from ' + rootPackage + '.core import blackbox\n')
         #if self.solver is not None:
-        #    blackboxFile.write('from ' + rootPackage + '.Solvers import ' + self.solver.name + '\n')
-        #blackboxFile.write('from ' + self.modelEvaluator.model.moduleName + ' import '+ self.modelEvaluator.model.objFuncName + '\n')
+        #    bb.write('from ' + rootPackage + '.Solvers import ' + \
+        #             self.solver.name + '\n')
+        #bb.write('from ' + self.modelEvaluator.model.moduleName + \
+        #         ' import '+ self.modelEvaluator.model.objFuncName + '\n')
         #for constraint in self.modelEvaluator.model.constraintNames:
-        #    blackboxFile.write('from ' + self.modelEvaluator.model.moduleName + ' import '+ constraint + '\n')
-        blackboxFile.write('# load the test data\n')
-        blackboxFile.write('try:\n')
-        blackboxFile.write(tab+'blackboxDataFile = open("' + self.model.data_file + '","r")\n')
-        blackboxFile.write(tab+'blackboxModel = pickle.load(blackboxDataFile)\n')
-        blackboxFile.write(tab+'blackboxDataFile.close()\n')
-        blackboxFile.write('except TypeError:\n')
-        blackboxFile.write(tab+'print "Error in loading"\n')
-        blackboxFile.write('blackbox = NOMADBlackbox(model=blackboxModel)\n')
-        #blackboxFile.write('blackbox.opt_data.synchronize_measures()\n')
-        blackboxFile.write('blackbox.run(*sys.argv)\n')
-        #blackboxFile.write('try:\n')
-        #blackboxFile.write(tab+'blackboxDataFile = open("' + self.dataFileName + '","w")\n')
-        #blackboxFile.write(tab+'pickle.dump(blackbox,blackboxDataFile)\n')
-        #blackboxFile.write(tab+'blackboxDataFile.close()\n')
-        #blackboxFile.write('except TypeError:\n')
-        #blackboxFile.write(tab+'print "Error in loading"\n')
-        blackboxFile.write('blackboxModel.save()\n')
-        #blackboxFile.write('blackboxRunLogFile.close()\n')
-        blackboxFile.close()
+        #    bb.write('from ' + self.modelEvaluator.model.moduleName + \
+        #             ' import '+ constraint + '\n')
+
+        bb.write('# Load test data.\n')
+        bb.write('try:\n')
+        bb.write(tab + 'blackboxDataFile = open("' + \
+                self.model.data_file + '","r")\n')
+        bb.write(tab + 'blackboxModel = pickle.load(blackboxDataFile)\n')
+        bb.write(tab+'blackboxDataFile.close()\n')
+        bb.write('except TypeError:\n')
+        bb.write(tab+'print "Error in loading"\n')
+        bb.write('blackbox = NOMADBlackbox(model=blackboxModel)\n')
+        #bb.write('blackbox.opt_data.synchronize_measures()\n')
+        bb.write('blackbox.run(*sys.argv)\n')
+        #bb.write('try:\n')
+        #bb.write(tab + 'blackboxDataFile = open("' + self.dataFileName + \
+        #         '", "w")\n')
+        #bb.write(tab+'pickle.dump(blackbox,blackboxDataFile)\n')
+        #bb.write(tab+'blackboxDataFile.close()\n')
+        #bb.write('except TypeError:\n')
+        #bb.write(tab+'print "Error in loading"\n')
+        bb.write('blackboxModel.save()\n')
+        #bb.write('blackboxRunLogFile.close()\n')
+        bb.close()
         #os.chmod(self.runFileName,0755)
         return
     
@@ -146,7 +177,7 @@ class NOMADSolver(Solver):
         self.blackbox.generate_executable_file()
         if surrogate is not None:
             surrogate.generate_executable_file()
-#            surrogate.save()
+        #   surrogate.save()
         self.initialize()
         self.run()
         return
@@ -154,43 +185,40 @@ class NOMADSolver(Solver):
     def initialize(self):
         "Write NOMAD config to file based on parameter optimization problem."
 
-        descriptionFile = open(self.paramFileName, "w")
+        descrFile = open(self.paramFileName, "w")
 
-        # Create problem descriptions basing on
-        # the model of blackbox
         if self.blackbox.model is not None:
             model = self.blackbox.model
-            descriptionFile.write('DIMENSION ' + str(model.n_var) + '\n')
-            # descriptionFile.write('DISPLAY_DEGREE 4\n')
-            descriptionFile.write('DISPLAY_STATS EVAL& BBE & SOL&  &OBJ \\\\ \n')
-            descriptionFile.write('BB_EXE "$python ' + self.blackbox.file_name + '"\n')
-            # descriptionFile.write('BB_EXE ' + blackbox.executableFileName + '\n')
+            descrFile.write('DIMENSION ' + str(model.n_var) + '\n')
+            # descrFile.write('DISPLAY_DEGREE 4\n')
+            descrFile.write('DISPLAY_STATS EVAL & BBE & SOL & & OBJ \\\\ \n')
+            descrFile.write('BB_EXE "$python ' + \
+                    self.blackbox.file_name + '"\n')
             bbTypeStr = 'BB_OUTPUT_TYPE OBJ'
             for i in range(model.m_con):
                 bbTypeStr = bbTypeStr + ' PB'
-            descriptionFile.write(bbTypeStr + '\n')
+            descrFile.write(bbTypeStr + '\n')
             surrogate = self.blackbox.surrogate
             if surrogate is not None:
-                descriptionFile.write('SGTE_EXE "$python ' + surrogate.file_name + '"\n')
+                descrFile.write('SGTE_EXE "$python ' + \
+                        surrogate.file_name + '"\n')
             pointStr = str(model.initial_points)
-            descriptionFile.write('X0 ' +  pointStr.replace(',',' ') + '\n')
-            #print 'NOMAD.py',[bound[0] for bound in blackbox.bounds]
+            descrFile.write('X0 ' +  pointStr.replace(',',' ') + '\n')
             lowerBoundStr = str([bound[0] for bound in model.bounds]).replace('None','-').replace(',',' ')
             upperBoundStr = str([bound[1] for bound in model.bounds]).replace('None','-').replace(',',' ')
-            descriptionFile.write('LOWER_BOUND ' + lowerBoundStr + '\n')
-            descriptionFile.write('UPPER_BOUND ' + upperBoundStr + '\n')
-        # Write the other settings
-        descriptionFile.write('SOLUTION_FILE ' + self.solutionFileName + '\n')
-        descriptionFile.write('STATS_FILE ' + self.resultFileName + ' EVAL& BBE & BBO & SOL&  &OBJ \\\\ \n')
+            descrFile.write('LOWER_BOUND ' + lowerBoundStr + '\n')
+            descrFile.write('UPPER_BOUND ' + upperBoundStr + '\n')
+
+        # Write other settings.
+        descrFile.write('SOLUTION_FILE ' + self.solutionFileName + '\n')
+        descrFile.write('STATS_FILE ' + self.resultFileName + \
+                ' EVAL & BBE & BBO & SOL & & OBJ \\\\ \n')
         for param_setting in self.parameter_settings:
-            descriptionFile.write(param_setting + '\n')
-        descriptionFile.close()
+            descrFile.write(param_setting + '\n')
+        descrFile.close()
         return
 
     def set_parameter(self, name=None, value=None):
-        #descriptionFile = open(self.paramFileName,'a')
-        #descriptionFile.write(param.str() + '\n')
-        #descriptionFile.close()
         param = Parameter(name=name,value=value)
         self.parameter_settings.append(param.str())
         return
