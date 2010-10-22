70a2be38e543fa62fe9a5bd326f8664b7aceaadb
Dominique Orban <dominique.orban@gmail.com>
2010-10-21 21:53:15 -0700

    Removed commented lined in fd_run.

--- examples/fd/fd_run.py
+++ examples/fd/fd_run.py
@@ -1,7 +1,6 @@
-from fd import fd
 from opal.core.io import *
+from fd import fd
 from math import pi, sin, cos
-import pickle
 import sys
 
 f = sin ; df = cos  # Target function and its derivative.
@@ -11,12 +10,6 @@ dfx = df(x)  # "Exact" derivative at x.
 def run(param_file, problem):
     "Run FD with given parameters."
 
-    #pf = open(param_file, 'rb')
-    #try:
-    #    parms = pickle.load(pf)
-    #except:
-    #    raise IOError, 'Parameter file does not have expected format'
-    #pf.close()
     params = read_params_from_file(param_file)
     h = params['h']
     return {'ERROR': abs(dfx - fd(f,x,h))}
@@ -27,13 +20,7 @@ if __name__ == '__main__':
     problem     = sys.argv[2]
     output_file = sys.argv[3]
 
-    # Solve and gather measures.
+    # Solve, gather measures and write to file.
     measures = run(param_file, problem)
-
-    # Write measures to file.
     write_measures_to_file(output_file, measures)
-    #f = open(output_file, 'w')
-    #for measure in measure_vals:
-    #    print >> f, measure, measure_vals[measure]
-    #f.close()
 
