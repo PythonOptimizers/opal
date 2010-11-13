aaa84b9e965c96c0ca73199ea33e2d2f7e5f37d7
Dominique Orban <dominique.orban@gmail.com>
2010-11-13 00:03:38 -0800

    Add new tools module.

---
+++ opal/core/tools.py
@@ -0,0 +1,22 @@
+# Miscellaneous tools.
+
+def extract_measure(content, description, name, valueType = 'int'):
+    numberPattern = {'int':'\d+',
+                     'real':'-?\d*\.\d+',
+                     'float':'-?\d*\.\d+E(\+|-)\d+'}
+    converters = {'int':int,
+                  'real': float,
+                  'float': float}
+
+    matches = re.finditer(description + '\s+:\s+(?P<' +
+                          name + '>' +
+                          numberPattern[valueType] + ')',
+                          content)
+    value = None
+    for m in matches:
+        if name not in m.groupdict().keys():
+            raise Exception('Could not to extract measure ' + name)
+            continue
+        value = converters[valueType](m.groupdict()[name])
+    return value
+
