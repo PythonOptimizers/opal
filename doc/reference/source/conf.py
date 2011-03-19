ede87e2598545f306e8e90b3b24b51d62e3b36fc
Dominique Orban <dominique.orban@gmail.com>
2011-03-19 12:28:51 -0400

    Name current working path.

--- doc/reference/source/conf.py
+++ doc/reference/source/conf.py
@@ -19,7 +19,8 @@ import sys, os
 # If your extensions are in another directory, add it here. If the directory
 # is relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
-sys.path.append(os.path.abspath('.'))
+here=os.path.abspath('.')
+sys.path.append(here)
 sys.path.append('sphinxext')
 
 # Import support for ipython console session syntax highlighting (lives
