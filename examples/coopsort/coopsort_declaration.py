66098f95195f518752ec9d486ea2566b75ce1242
Dominique Orban <dominique.orban@gmail.com>
2012-12-19 19:41:04 -0500

    Clean up Coopsort declaration.

--- examples/coopsort/coopsort_declaration.py
+++ examples/coopsort/coopsort_declaration.py
@@ -2,22 +2,23 @@ from opal.core.algorithm import Algorithm
 from opal.core.parameter import Parameter
 from opal.core.measure import Measure
 
-
 # Define new algorithm.
 coopsort = Algorithm(name='CoopSort', description='Sort Algorithm')
 
-# Register executable for .
+# Register executable.
 coopsort.set_executable_command('python coopsort_run.py')
 
 # Define parameters.
 
-# 5. Line search parameters
-
+# The following coop tree amounts to 5522522 (in base 6.)
 coopsort.add_param(Parameter(name='coopTree',
                              kind='categorical',
-                             default=284354431,
+                             default=275378,
                              description='Encoded cooperation tree'))
 
+# This dummy parameter is just there to circumvent a bug in NOMAD
+# that occurs when the problem has a single parameter and this parameter
+# is categorical.
 coopsort.add_param(Parameter(name='nothing',
                              kind='integer',
                              default=0,
